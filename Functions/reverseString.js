880d418909785a7e4502a1b6b702d2cc93452509
João Soranz <joaosoranz@yahoo.com.br>
2019-02-25 10:05:55 +1000

    reverse string

--- Functions/reverseString.js
+++ Functions/reverseString.js
@@ -13,3 +13,22 @@ function reverse(str) {
   
   console.log(reverse(435435));
   console.log(reverse('Joao Arantes Soranz'));
+
+  function reverse2(str) {
+
+    //check input
+    if(!str || str.length < 2 || typeof str !== 'string')
+      return str;
+  
+    const backwards = [];
+    const totalItems = str.length - 1;
+  
+    for(let i = totalItems; i >= 0; i--) {
+      backwards.push(str[i]);    
+    }
+  
+    return backwards.join('');
+  }
+  
+  reverse2(435435);
+  reverse2('Hey my name is John');
